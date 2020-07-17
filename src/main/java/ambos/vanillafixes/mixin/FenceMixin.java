049d85b551555485990e29453f56464c38177581
Amb0s <ambos@disroot.org>
2020-07-17 14:12:15 +0200

    Minor addition/changes
    - Renamed methods
    - Changed indentation
    - Added a new resources proxy
    - Fixed fences placement

---
+++ src/main/java/ambos/vanillafixes/mixin/FenceMixin.java
@@ -0,0 +1,16 @@
+package ambos.vanillafixes.mixin;
+
+import net.minecraft.level.Level;
+import net.minecraft.tile.Fence;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(Fence.class)
+final class FenceMixin {
+    @Inject(method = "canPlaceAt", at = @At("RETURN"), cancellable = true)
+    private void onCanPlaceAt(Level level, int x, int y, int z, CallbackInfoReturnable cir) {
+        cir.setReturnValue(true);
+    }
+}
