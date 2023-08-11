f7e96ccbb47cb26a48cb33fb97007cf9c6686f80
Amb0s <ambos@disroot.org>
2023-08-11 09:33:44 +0200

    Removed useless mixin and changed directory path

--- src/main/java/turniplabs/vanillafixes/mixin/SoundManagerMixin.java
+++ src/main/java/turniplabs/vanillafixes/mixin/SoundManagerMixin.java
@@ -1,10 +1,13 @@
 package turniplabs.vanillafixes.mixin;
 
+import net.minecraft.client.Minecraft;
 import net.minecraft.client.sound.SoundManager;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.ModifyArgs;
 import org.spongepowered.asm.mixin.injection.invoke.arg.Args;
+
+import java.io.File;
 
 @Mixin(value = SoundManager.class, remap = false)
 final class SoundManagerMixin {
@@ -12,14 +15,15 @@ final class SoundManagerMixin {
             target = "Lnet/minecraft/client/sound/SoundManager;loadModAudio(Ljava/lang/String;Lnet/minecraft/client/sound/SoundPool;)V"),
             require = 0)
     private void renameMinecraftdirectory(Args args) {
+
         if (args.get(0) == "minecraft-bta/resources/mod/sound") {
-            args.set(0, "bta/resources/mod/sound");
+            args.set(0, "minecraft/resources/mod/sound");
         } else if (args.get(0) == "minecraft-bta/resources/mod/streaming") {
-            args.set(0, "bta/resources/mod/streaming");
+            args.set(0, "minecraft/resources/mod/streaming");
         } else if (args.get(0) == "minecraft-bta/resources/mod/music") {
-            args.set(0, "bta/resources/mod/music");
+            args.set(0, "minecraft/resources/mod/music");
         } else {
-            args.set(0, "bta/resources/mod/cavemusic");
+            args.set(0, "minecraft/resources/mod/cavemusic");
         }
     }
 }
